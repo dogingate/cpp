c04d72ffb4dc90f7298203961751ff006b9de10b
vibration007 <dogingate@qq.com>
2022-05-13 17:55:34 +0800

    office

---
+++ flie.h
@@ -0,0 +1,12 @@
+/*
+ * @Author: vibration007 dogingate@qq.com
+ * @Date: 2022-05-13 17:50:36
+ * @LastEditors: vibration007 dogingate@qq.com
+ * @LastEditTime: 2022-05-13 17:50:43
+ * @FilePath: \cpp\flie.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#pragma once
+class File
+{
+};
